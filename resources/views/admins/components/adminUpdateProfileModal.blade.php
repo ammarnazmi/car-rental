359f5214336a922e2b80c2f1c7af819628988005
ammar <ammarnazmi44@gmail.com>
2024-01-10 14:21:26 +0800

    feature

--- resources/views/admins/components/adminUpdateProfileModal.blade.php
+++ resources/views/admins/components/adminUpdateProfileModal.blade.php
@@ -1,24 +1,59 @@
-<div class="modal fade" id="updateProfile" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog ">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Profile</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <div class="form-group">
-                    <label for="username">Username</label>
-                    <input id="username" class="form-control" type="text" name="username" value="Name">
+<div class="modal fade" id="updateProfileModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <form action="" method="post" id="updateProfileForm">
+        @csrf
+        <input type="hidden" id="adminId">
+        <div class="modal-dialog ">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Profile</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input id="email" class="form-control" type="text" name="email" value="Name">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input id="updateUsername" class="form-control" type="text" name="update_username">
+                        <div class="updateUsernameError"></div>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input id="updateEmail" class="form-control" type="text" name="update_email">
+                    </div>
                 </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                <button type="button" class="btn btn-primary">Save</button>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn btn-primary">Save</button>
+                </div>
             </div>
         </div>
-    </div>
+    </form>
 </div>
+
+<script>
+    $(document).ready(function() {
+        $('#updateProfileForm').submit(function(event) {
+            event.preventDefault();
+
+            var adminId = $('#adminId').val();
+            var updateUsername = $('#updateUsername').val();
+            var updateEmail = $('#updateEmail').val();
+
+            $.ajax({
+                url: '/api/admins/' + adminId + '/update-profile',
+                method: 'PUT',
+                data: {
+                    name: updateUsername,
+                    email: updateEmail,
+                },
+                success: function(response) {
+                    $('#updateProfileModal').modal('hide');
+                    $('#username').val(updateUsername);
+                    $('#username').val(updateUsername);
+                    $('.toast').toast('show');
+                },
+                error: function(error) {
+                    console.log('error');
+                }
+            });
+        });
+    })
+</script>
